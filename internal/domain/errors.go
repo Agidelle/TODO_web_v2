6e82cd8e372beed4fe193854042ebe5901ea043f
Agidelle <agidelleds@gmail.com>
2025-08-24 01:10:28 +0300

    complete service

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -1,1 +1,24 @@
 package domain
+
+import "errors"
+
+var (
+	ErrID             = errors.New("некорректный id")
+	ErrBadTitle       = errors.New("не указан заголовок задачи")
+	ErrDate           = errors.New("неправильный формат даты")
+	ErrInternalServer = errors.New("внутренняя ошибка сервера")
+)
+
+type CustomError struct {
+	Code       int
+	Err        error
+	ErrStorage error
+}
+
+func NewCustomError(code int, err error, errStorage error) *CustomError {
+	return &CustomError{
+		Code:       code,
+		Err:        err,
+		ErrStorage: errStorage,
+	}
+}
